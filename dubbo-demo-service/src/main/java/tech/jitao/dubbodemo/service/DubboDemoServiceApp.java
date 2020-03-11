bb979a481e70ae39c6de9682f302b6e990632283
jitao <flmnix@163.com>
2020-03-11 18:20:58 +0800

    ~

--- dubbo-demo-service/src/main/java/tech/jitao/dubbodemo/service/DubboDemoServiceApp.java
+++ dubbo-demo-service/src/main/java/tech/jitao/dubbodemo/service/DubboDemoServiceApp.java
@@ -1,9 +1,11 @@
 package tech.jitao.dubbodemo.service;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo(scanBasePackages = "tech.jitao.dubbodemo.service.service")
 public class DubboDemoServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(DubboDemoServiceApp.class, args);
